0ec71151c97d6f85c52d1cdabb8d26fbaf2787f7
Vipulpandey007 <vipulpandey708@gmail.com>
2024-02-06 19:01:15 +0530

    first commit

---
+++ app/(Site)/components/AuthForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useState } from "react";
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState();
+  return <div>Auth2</div>;
+};
+export default AuthForm;
